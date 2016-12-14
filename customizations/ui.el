86b40639f21a874dee4d5ae46a7bcdeedc1d6c25
aleczhang <jun850605@gmail.com>
2016-12-14 13:17:44 +0800

    solarized light theme

--- customizations/ui.el
+++ customizations/ui.el
@@ -24,9 +24,10 @@
 ;; for a great explanation of emacs color themes.
 ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Custom-Themes.html
 ;; for a more technical explanation.
+(set-frame-parameter nil 'background-mode 'light)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (add-to-list 'load-path "~/.emacs.d/themes")
-(load-theme 'tomorrow-night-bright t)
+(load-theme 'solarized t)
 
 ;; increase font size for better readability
 (set-face-attribute 'default nil :height 130)
