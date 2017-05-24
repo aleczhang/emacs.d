1161be32cffbd41b478adad2eb71261768fbff09
aleczhang <jun850605@gmail.com>
2017-05-24 12:06:27 +0800

    1. update cider plugin to 0.14.0 2. correct melba package location 3. remove marmalade & tromey package repository

--- elpa/cider-0.14.0/cider-pkg.el
+++ elpa/cider-0.14.0/cider-pkg.el
@@ -1,4 +1,4 @@
-(define-package "cider" "20161009.212" "Clojure Interactive Development Environment that Rocks"
+(define-package "cider" "0.14.0" "Clojure Interactive Development Environment that Rocks"
   '((emacs "24.3")
     (clojure-mode "5.5.2")
     (pkg-info "0.4")
